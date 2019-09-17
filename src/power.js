2699ade356391713f2bab94384c77aedc2a2967e
unknown <[shrestha_sl@hotmail.com]>
2019-09-16 19:00:41 -0500

    activate and deactivate features added

--- src/power.js
+++ src/power.js
@@ -5,20 +5,54 @@ document.querySelector("#activate-flight").addEventListener("click", () => {
 flightHandler = () => {
   document.querySelector("#flight").classList.replace("disabled", "enabled");
 };
+flightHandlerDisable = () => {
+  document.querySelector("#flight").classList.replace("enabled", "disabled");
+};
 
-document.querySelector("#activate-mindreading").addEventListener("click", () => {
+document
+  .querySelector("#activate-mindreading")
+  .addEventListener("click", () => {
     flightMindReading();
-
-})
+  });
 
 flightMindReading = () => {
-    document.querySelector("#mindreading").classList.replace("disabled", "enabled");
-}
+  document
+    .querySelector("#mindreading")
+    .classList.replace("disabled", "enabled");
+};
 
+flightMindReadingDisable = () => {
+  document
+    .querySelector("#mindreading")
+    .classList.replace("enabled", "disabled");
+};
 document.querySelector("#activate-xray").addEventListener("click", () => {
-    flightxray();
-})
+  flightxray();
+});
 
 flightxray = () => {
-    document.querySelector("#xray").classList.replace("disabled", "enabled");
-}
+  document.querySelector("#xray").classList.replace("disabled", "enabled");
+};
+flightxrayDisable = () => {
+  document.querySelector("#xray").classList.replace("enabled", "disabled");
+};
+
+document.querySelector("#activate-all").addEventListener("click", () => {
+  activateAll();
+});
+
+activateAll = () => {
+  flightHandler();
+  flightMindReading();
+  flightxray();
+};
+
+document.querySelector("#deactivate-all").addEventListener("click", () => {
+  deactivateAll();
+});
+
+deactivateAll = () => {
+  flightHandlerDisable();
+  flightMindReadingDisable();
+  flightxrayDisable();
+};
